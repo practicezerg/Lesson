1521c923c937b50cf970f14bd088426489be728f
practicezerg <unknown_rus@mail.ru>
2022-10-29 02:16:15 +0300

    29.10

--- lesson21 string practice.py
+++ lesson21 string practice.py
@@ -1,19 +1,19 @@
 # ближе к сердцу
-text = input("Input words")
-small = []
-big = []
-n = len(text)
-print(n)
-for i in text:
-    if "a" <= i <= "z":
-        small.append(i)
-for i in text:
-    if "A" <= i <= "Z":
-        big.append(i)
-if len(small) >= len(big):
-    print(text.lower())
-else:
-    print(text.upper())
+# text = input("Input words")
+# small = []
+# big = []
+# n = len(text)
+# print(n)
+# for i in text:
+#     if "a" <= i <= "z":
+#         small.append(i)
+# for i in text:
+#     if "A" <= i <= "Z":
+#         big.append(i)
+# if len(small) >= len(big):
+#     print(text.lower())
+# else:
+#     print(text.upper())
 
 # у верхнего недоработка потому что рассматривается тольк  английский вариант
 # ==================================
@@ -40,4 +40,56 @@ else:
 #     print(text.upper())
 
 
+# ==================================
+def try_unput():
+    test = input("Input 2 numbers with . =")
+    return test
 
+def telo(vvod):
+    for i in vvod:
+        number_ornot = vvod.isdigit()
+        if number_ornot == True:
+            a, b = vvod.split(".")
+            a = int(a)
+            b = int(b)
+            print(a + b)
+        else:
+            try_unput()
+
+
+
+vvod = try_unput()
+print(vvod)
+telo(vvod)
+# ==================================
+text = "4e"
+while text.isdigit() == False:
+    text = input("Input 2 numbers=        ")
+a = int(text[0])
+b = int(text[1])
+print(a + b)
+
+# ==================================
+# text = "45"
+# a = int(text[0])     само суммирование
+# b = int(text[1])
+# print(a + b)
+#
+# ============
+# print(text.isdigit())   проверка на цифры
+# ++++++++++++
+def test1():
+    qq = input("")
+    return qq
+
+
+def test2(text):
+    if text.isdigit() == True:
+        a = int(text[0])
+        b = int(text[1])
+        print(a + b)
+    else:
+        test2()
+
+text = test1()
+test2(text)
---
+++ lesson22 tuple prac.py
@@ -0,0 +1,9 @@
+# list = [10, 2.13, 'square', 89, 'C', 'xerase', 'stepa']
+# list2 = list.copy()
+# list3 = list[:]
+# list2.append("milk")
+# list3.pop(2)
+# print(list)
+# print(list2)
+# print(list3)
+# =====================================
