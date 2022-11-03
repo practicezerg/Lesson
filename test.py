1cac0ec835db514bbd8258b77d03bc624bfafa70
practicezerg <unknown_rus@mail.ru>
2022-11-03 04:21:03 +0300

    03.11

--- test.py
+++ test.py
@@ -1,14 +1,15 @@
-num = 5
-
-g = num % 2
-print(g)
-if int(num / 2):
-    print("OK")
-else:
-    print("no")
-
-
-# print(test)
 # my_str = '01234567890'
 # print(my_str.endswith('0123', 0,4))
 # print(my_str.endswith("0"))
+
+string_ = ("What are you, a communist?")
+string_ = list(string_)
+print(string_)
+#a i o e u
+for qq in string_:
+    if qq == "i" or qq == "a" or qq == "o" or qq == "e" or qq == "u" or qq == "I" or qq == "A" or qq == "O" or qq == "E" or qq == "U":
+        string_.remove(qq)
+    else:
+        continue
+string_ = "".join(string_)
+print(string_)
---
+++ test2.py
@@ -0,0 +1,11 @@
+a = "sdf dfsd 0 asda 1 asdasd 4  23f34fsdf"
+b = a.split(" ")
+print(b)
+g = 0
+for i in b:
+    if b.isdigit[0:] == True:
+        i = int(i)
+        g = g + i
+    else:
+        continue
+print(g)
