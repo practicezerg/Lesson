4d6e6ca826eb8f3600ec464912853263f7285427
JunG <unknown_rus@mail.ru>
2022-11-18 16:43:57 +0300

    18.11 16-43

--- parcing/w003_parsing_1.py
+++ parcing/w003_parsing_1.py
@@ -1,1 +1,10 @@
-https://news.ru/
+# https://news.ru/
+import requests
+from bs4 import BeautifulSoup as BS
+
+r = requests.get("https://stopgame.ru/review/new/izumitelno/p1")
+html = BS(r.content, "html.parser")
+
+for el in html.select(".items > .article-summary"):
+    title = el.select(".caption > a")
+    print(title[0].text)
