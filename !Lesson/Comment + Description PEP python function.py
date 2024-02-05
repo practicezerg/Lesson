1a9b6d9dec33dbc50e7466a8e23abe0a1554be83
practicezerg <116392226+practicezerg@users.noreply.github.com>
2024-02-05 10:05:16 +0300

    Create Comment + Description PEP python function.py

---
+++ !Lesson/Comment + Description PEP python function.py
@@ -0,0 +1,14 @@
+Описания функций в Python
+
+def any_func(param1, param2):
+	"""
+	Описание функции
+	
+	:param param1: Описание параметра 1
+	:param param2: Описание параметра 2
+	:return: Описание возвращаемого значения
+    """
+	# Код функции
+
+
+def add_numbers(a: int, b: int) -> int:
