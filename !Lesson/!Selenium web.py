32ddd8f2dfed78bf4a89302d9b0658e9754d7880
practicezerg <116392226+practicezerg@users.noreply.github.com>
2024-05-04 05:46:01 +0300

    Update !Selenium web.py

---
+++ !Lesson/!Selenium web.py
@@ -0,0 +1,17 @@
+заметки по Selenium
+
+with Driver(headed=False) as driver:
+			driver.maximize_window()
+			# Устанавливаем максимальное время ожидания в 30 секунд для всех элементов
+			driver.implicitly_wait(30)
+			# создаем всю информацию котоаря нужна при регистрации
+
+
+сделать скриншот, иногда помогает при отлове ошибки 
+driver.save_screenshot("path_for_file/name_file.png")
+
+выполнение скрипта
+driver.execute_script(f"window.scrollBy(0, {pixels_to_scroll});")
+
+передаем страничку в bs для парсинга
+soup = BS(driver.page_source, features="html.parser")
